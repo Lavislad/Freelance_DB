4d30b7cdd52a117b9c366ab73fc755dd04af2b3f
Vladislav <vladislav.mnac@gmail.com>
2026-05-15 18:17:10 +0300

    Init

---
+++ FreelanceExchange/LoginForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FreelanceExchange
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+
+        private System.Windows.Forms.ComboBox cmbRole;
+
+        private System.Windows.Forms.Button btnLogin;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+
+            this.btnLogin = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+
+            // label1
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.Text = "Логин";
+
+            // txtLogin
+            this.txtLogin.Location = new System.Drawing.Point(140, 35);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(180, 23);
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.Text = "Пароль";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(140, 85);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(180, 23);
+
+            // label3
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.Text = "Роль";
+
+            // cmbRole
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(140, 135);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(180, 23);
+            cmbRole.Items.Add("Администратор");
+            cmbRole.Items.Add("Заказчик");
+            cmbRole.Items.Add("Фрилансер");
+
+            cmbRole.SelectedIndex = 0;
+
+            // btnLogin
+            this.btnLogin.Location = new System.Drawing.Point(140, 190);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(120, 35);
+            this.btnLogin.Text = "Войти";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+
+            // LoginForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 280);
+
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtPassword);
+
+            this.Controls.Add(this.cmbRole);
+
+            this.Controls.Add(this.btnLogin);
+
+            this.Name = "LoginForm";
+            this.Text = "Авторизация";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ FreelanceExchange/LoginForm.cs
@@ -0,0 +1,64 @@
+﻿using Npgsql;
+using System;
+using System.Windows.Forms;
+
+namespace FreelanceExchange
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string connectionString = "";
+
+            if (cmbRole.Text == "Администратор")
+            {
+                connectionString =
+                    "Host=localhost;" +
+                    "Port=5432;" +
+                    "Database=freelance_db;" +
+                    "Username=admin_role;" +
+                    "Password=admin123;";
+            }
+
+            else if (cmbRole.Text == "Заказчик")
+            {
+                connectionString =
+                    "Host=localhost;" +
+                    "Port=5432;" +
+                    "Database=freelance_db;" +
+                    "Username=customer_role;" +
+                    "Password=customer123;";
+            }
+
+            else if (cmbRole.Text == "Фрилансер")
+            {
+                connectionString =
+                    "Host=localhost;" +
+                    "Port=5432;" +
+                    "Database=freelance_db;" +
+                    "Username=freelancer_role;" +
+                    "Password=free123;";
+            }
+
+            try
+            {
+                using (NpgsqlConnection connection =
+                       new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    MessageBox.Show("Подключение успешно!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
