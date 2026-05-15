ba74a356064aba3671dc088fe7c7b62698bac550
Vladislav <vladislav.mnac@gmail.com>
2026-05-15 19:29:59 +0300

    login feature is done

--- init.sql
+++ init.sql
@@ -2,27 +2,18 @@
 
 -- DROP DATABASE IF EXISTS postgres;
 
--- CREATE DATABASE postgres
---     WITH
---     OWNER = postgres
---     ENCODING = 'UTF8'
---     LC_COLLATE = 'Russian_Russia.1251'
---     LC_CTYPE = 'Russian_Russia.1251'
---     LOCALE_PROVIDER = 'libc'
---     TABLESPACE = pg_default
---     CONNECTION LIMIT = -1
---     IS_TEMPLATE = False;
-
--- COMMENT ON DATABASE postgres
+CREATE DATABASE freelance_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+-- COMMENT ON DATABASE freelance_db
 --     IS 'default administrative connection database';
-
-CREATE TABLE responses (
-    id SERIAL PRIMARY KEY,
-    vacancy_id INTEGER REFERENCES vacancies(id) ON DELETE CASCADE,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    message TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
@@ -51,6 +42,14 @@ CREATE TABLE vacancies (
     deadline DATE,
     publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     author_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE responses (
+    id SERIAL PRIMARY KEY,
+    vacancy_id INTEGER REFERENCES vacancies(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    message TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tags (
