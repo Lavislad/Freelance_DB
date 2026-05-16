7d39417b7ca5893b937f621f461fd1241f72f861
Vladislav <vladislav.mnac@gmail.com>
2026-05-16 13:23:23 +0300

    Login form enchanced

--- FreelanceExchange/LoginForm.Designer.cs
+++ FreelanceExchange/LoginForm.Designer.cs
@@ -6,12 +6,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
 
         private System.Windows.Forms.TextBox txtLogin;
         private System.Windows.Forms.TextBox txtPassword;
-
-        private System.Windows.Forms.ComboBox cmbRole;
 
         private System.Windows.Forms.Button btnLogin;
 
@@ -29,10 +26,8 @@
         {
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
-            label3 = new System.Windows.Forms.Label();
             txtLogin = new System.Windows.Forms.TextBox();
             txtPassword = new System.Windows.Forms.TextBox();
-            cmbRole = new System.Windows.Forms.ComboBox();
             btnLogin = new System.Windows.Forms.Button();
             button1 = new System.Windows.Forms.Button();
             SuspendLayout();
@@ -55,15 +50,6 @@
             label2.TabIndex = 1;
             label2.Text = "Пароль";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(40, 140);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(34, 15);
-            label3.TabIndex = 2;
-            label3.Text = "Роль";
-            // 
             // txtLogin
             // 
             txtLogin.Location = new System.Drawing.Point(140, 35);
@@ -79,18 +65,9 @@
             txtPassword.Size = new System.Drawing.Size(141, 23);
             txtPassword.TabIndex = 4;
             // 
-            // cmbRole
-            // 
-            cmbRole.FormattingEnabled = true;
-            cmbRole.Items.AddRange(new object[] { "Администратор", "Заказчик", "Фрилансер" });
-            cmbRole.Location = new System.Drawing.Point(140, 135);
-            cmbRole.Name = "cmbRole";
-            cmbRole.Size = new System.Drawing.Size(180, 23);
-            cmbRole.TabIndex = 5;
-            // 
             // btnLogin
             // 
-            btnLogin.Location = new System.Drawing.Point(140, 190);
+            btnLogin.Location = new System.Drawing.Point(140, 140);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new System.Drawing.Size(120, 35);
             btnLogin.TabIndex = 6;
@@ -110,18 +87,21 @@
             // 
             // LoginForm
             // 
+            AcceptButton = btnLogin;
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(400, 280);
+            ClientSize = new System.Drawing.Size(400, 206);
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(label2);
-            Controls.Add(label3);
             Controls.Add(txtLogin);
             Controls.Add(txtPassword);
-            Controls.Add(cmbRole);
             Controls.Add(btnLogin);
+            ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            MaximumSize = new System.Drawing.Size(416, 245);
+            MinimumSize = new System.Drawing.Size(416, 245);
             Name = "LoginForm";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             Text = "Авторизация";
             ResumeLayout(false);
             PerformLayout();
