b16e2517381a396929564d1d53bc2afd12c521da
Vladislav <vladislav.mnac@gmail.com>
2026-05-17 15:33:49 +0300

    Test project created

---
+++ TestFreelanceExchange/MainFormTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestFreelanceExchange
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TestFreelanceExchange/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d1c1dfc-3595-43fd-97a2-c38ed62ad20a")]
